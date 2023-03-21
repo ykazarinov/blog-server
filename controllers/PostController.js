bca0469c3f6a668e367de7614badd1a9516767de
Ян Ян <kazarinov.yanek@gmail.com>
2023-03-21 15:55:10 +0100

    cors + tags

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,5 +1,24 @@
 import PostSchema from '../models/Post.js'
 
+
+export const getLastTags = async(req, res) => {
+    try{
+        const posts = await PostSchema.find().limit(5).exec()
+        const tags = posts
+            .map(obj => obj.tags)
+            .flat()
+            .slice(0, 5)
+
+        res.json(tags)
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({
+            message: 'Failed to get posts'
+        })
+    }
+}
+
 export  const getOne = async (req, res) => {
     try{
         const postId = req.params.id 
--- index.js
+++ index.js
@@ -4,13 +4,16 @@ dotenv.config()
 import express from 'express'
 import mongoose from 'mongoose';
 
+import multer from 'multer'
+import cors from 'cors'
+
 
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
-import checkAuth from './utils/checkAuth.js';
 
-import * as UserController from './controllers/UserController.js'
-import * as PostController from './controllers/PostController.js'
+import {checkAuth, handleValidationErrors} from './utils/index.js';
+import {UserController, PostController} from './controllers/index.js'
+
 
 mongoose.connect(process.env.DB_ACCESS
 ).then(()=>{console.log('DB ok')
@@ -20,19 +23,43 @@ mongoose.connect(process.env.DB_ACCESS
 
 const app = express();
 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (__, file, cb) => {
+        cb(null, file.originalname)
+    },
+})
+
+const upload = multer({storage})
+
 app.use(express.json())
+app.use(cors())
+app.use('/uploads', express.static('uploads'))
 
 
 
-app.post('/auth/login', loginValidation, UserController.login)
-app.post('/auth/register', registerValidation, UserController.register)
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register',  registerValidation, handleValidationErrors, UserController.register)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+    res.jsonp({
+        url: `/uploads/${req.file.originalname}`
+    })
+})
+
+app.get('/tags', PostController.getLastTags)
+
 app.get('/posts', PostController.getAll)
+
+app.get('/posts/tags', PostController.getLastTags)
+
 app.get('/posts/:id', PostController.getOne)
-app.post('/posts', checkAuth, postCreateValidation,  PostController.create)
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
 app.delete('/posts/:id', checkAuth,  PostController.remove)
-app.patch('/posts/:id', checkAuth,  PostController.update)
+app.patch('/posts/:id', checkAuth, postCreateValidation, handleValidationErrors, PostController.update)
 
 
 
