71b833e7d5e25c0b36a54a73223c0fa4dab369e2
Ян Ян <kazarinov.yanek@gmail.com>
2023-03-24 14:50:34 +0100

    minor improvements

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -103,7 +103,7 @@ export const create = async (req, res) => {
             title: req.body.title,
             text: req.body.text,
             imageUrl: req.body.imageUrl,
-            tags: req.body.tags,
+            tags: req.body.tags.split(','),
             user: req.userId,
         })
 
@@ -130,7 +130,7 @@ export const update = async (req, res) => {
             text: req.body.text,
             imageUrl: req.body.imageUrl,
             user: req.userId,
-            tags: req.body.tags,
+            tags: req.body.tags.split(','),
 
         })
         if(updatePost){
